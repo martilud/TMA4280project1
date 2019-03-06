792d9bc7b8a2f3d18c09667e2aeab94d6981f673
martilud <martilud@stud.ntnu.no>
2019-03-07 00:12:31 +0100

    Final commit

--- results/plotResults.py
+++ results/plotResults.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+show = False
 if("PLOT CONVERGENCE PART 1"):
     filenames = ['zeta0.txt', 'mach0.txt']
     for file in filenames:
@@ -19,7 +19,9 @@ if("PLOT CONVERGENCE PART 1"):
     plt.xlabel(r"$n$")
     plt.ylabel(r"$|\pi_n - \pi|$")
     plt.savefig("Convergence1.pdf")
-    plt.show()
+    if show: plt.show()
+    plt.clf()
+
 
 if("PLOT TIME PART 1"):
     filenames = ['zeta0.txt', 'mach0.txt']
@@ -39,7 +41,8 @@ if("PLOT TIME PART 1"):
     plt.xlabel(r"$n$")
     plt.ylabel(r"Time in seconds")
     plt.savefig("Time1.pdf")
-    plt.show()
+    if show: plt.show()
+    plt.clf()
 
 if("PLOT CONVERGENCE PART 2"):
     filenames = ['zeta2.txt', 'mach2.txt']
@@ -66,7 +69,8 @@ if("PLOT CONVERGENCE PART 2"):
         plt.xlabel(r"$n$")
         plt.ylabel(r"$|\pi_n - \pi|$")
         plt.savefig(file.split('.')[0] + 'Convergence.pdf')
-        plt.show()
+        if show: plt.show()
+        plt.clf()
 
 if("PLOT TIME PART 2"):
     filenames = ['zeta2.txt', 'mach2.txt']
@@ -91,9 +95,10 @@ if("PLOT TIME PART 2"):
         plt.legend([str(keys) + "processes" for keys in ndict]) 
         plt.title("Time plot of " + file.split('.')[0])
         plt.xlabel(r"$n$")
-        plt.ylabel(r"$Time in seconds$")
+        plt.ylabel(r"Time in seconds")
         plt.savefig(file.split('.')[0] + 'Time.pdf')
-        plt.show()
+        if show: plt.show()
+        plt.clf()
 
 if("PLOT CONVERGENCE PART 3"):
     filenames = ['zeta3.txt', 'mach3.txt']
@@ -120,7 +125,8 @@ if("PLOT CONVERGENCE PART 3"):
         plt.xlabel(r"$n$")
         plt.ylabel(r"$|\pi_n - \pi|$")
         plt.savefig(file.split('.')[0] + 'Convergence.pdf')
-        plt.show()
+        if show: plt.show()
+        plt.clf()
 
 if("PLOT TIME PART 3"):
     filenames = ['zeta3.txt', 'mach3.txt']
@@ -145,7 +151,36 @@ if("PLOT TIME PART 3"):
         plt.legend([str(keys) + "processes" for keys in ndict]) 
         plt.title("Time plot of " + file.split('.')[0])
         plt.xlabel(r"$n$")
-        plt.ylabel(r"$Time in seconds$")
+        plt.ylabel(r"Time in seconds")
         plt.savefig(file.split('.')[0] + 'Time.pdf')
-        plt.show()
+        if show: plt.show()
+        plt.clf()
+
+if("PLOT TIME PART 4"):
+    filenames = ['zeta4.txt', 'mach4.txt']
+    for file in filenames:
+        data = open(file, 'r')
+        header = data.readline()
+        ndict = {} 
+        resultdict = {}
+        for line in data:
+            line = line.split()
+            if(line[0] in ndict):
+                ndict[line[0]].append(int(line[1]))
+            else:
+                ndict[line[0]] = [int(line[1])]
+            if(line[0] in resultdict):
+                resultdict[line[0]].append(float(line[3]))
+            else:
+                resultdict[line[0]] = [float(line[3])]
+        for key in ndict:
+            plt.loglog(ndict[key],resultdict[key])
+        plt.grid()
+        plt.legend([str(keys) + "processes" for keys in ndict]) 
+        plt.title("Time plot of " + file.split('.')[0])
+        plt.xlabel(r"$n$")
+        plt.ylabel(r"Time in seconds")
+        plt.savefig(file.split('.')[0] + 'Time.pdf')
+        if show: plt.show()
+        plt.clf()
 
